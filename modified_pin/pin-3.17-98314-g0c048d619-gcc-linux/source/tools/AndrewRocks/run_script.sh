c3c4fa571fbff2398c9406d066bac2668a897413
Emily Costa <costa.em@northeastern.edu>
2020-12-02 18:20:07 -0500

    stuff

--- modified_pin/pin-3.17-98314-g0c048d619-gcc-linux/source/tools/AndrewRocks/run_script.sh
+++ modified_pin/pin-3.17-98314-g0c048d619-gcc-linux/source/tools/AndrewRocks/run_script.sh
@@ -1,8 +1,11 @@
 // set this first!!!
-PIN_ROOT='/scratch/costa.em/pin-3.17-98314-g0c048d619-gcc-linux'
+PIN_ROOT='/scratch/costa.em/computer_architecture/modified_pin/pin-3.17-98314-g0c048d619-gcc-linux'
 
 // example
 // first, run program
-python hello_world.py
+//python hello_world.py
 // second, run using pin
-$PIN_ROOT/pin -t obj-intel64/pintool.so -- python hello_world.py
+//$PIN_ROOT/pin -t obj-intel64/pintool.so -- python hello_world.py > hello_world_trace.txt
+
+python3 transitive_closure_of_a_graph.py 
+$PIN_ROOT/pin -t obj-intel64/pintool.so -- python transitive_closure_of_a_graph.py 
